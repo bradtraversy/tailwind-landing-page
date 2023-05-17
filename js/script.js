a869ffb28d9742a956ff1b351d2c47366ab49d99
Irtaza Hussain <98938307+irtaza9@users.noreply.github.com>
2023-05-18 00:26:44 +0500

    Added Slider in testimonials, smoothscrolling

--- js/script.js
+++ js/script.js
@@ -1,8 +1,48 @@
-const btn = document.getElementById('menu-btn')
-const nav = document.getElementById('menu')
+const btn = document.getElementById("menu-btn");
+const nav = document.getElementById("menu");
 
-btn.addEventListener('click', () => {
-  btn.classList.toggle('open')
-  nav.classList.toggle('flex')
-  nav.classList.toggle('hidden')
-})
+btn.addEventListener("click", () => {
+  btn.classList.toggle("open");
+  nav.classList.toggle("flex");
+  nav.classList.toggle("hidden");
+});
+
+(function ($) {
+  "use strict";
+  $(".testimonials").slick({
+    dots: true,
+    autoplay: true,
+    infinite: true,
+    speed: 100,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 976,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ],
+  });
+})(jQuery);
